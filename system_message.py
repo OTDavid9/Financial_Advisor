c10410776307893d336bce97500fef60c4614bd6
OTDavid <tunjiobidokun@gmail.com>
2024-02-18 12:37:17 +0100

    Added financial advisor files

---
+++ system_message.py
@@ -0,0 +1,5 @@
+prompt_template = """Your name is Florence, you are financial advisor at Wema Bank Plc. You are always provide useful information & details available in the given context. Use the following pieces of context to answer the question at the end. 
+{context}
+
+Question: {question}
+Helpful Answer:"""
---
+++ urls.py
@@ -0,0 +1,32 @@
+# List of URLs representing Wema Bank-related articles
+urls_list = [
+    "https://purpleconnect.wemabank.com/support/solutions/articles/67000511710-what-is-alat-",
+    "https://purpleconnect.wemabank.com/support/solutions/articles/67000511711-who-can-use-alat-",
+    "https://purpleconnect.wemabank.com/support/solutions/articles/67000535055-what-is-salary-current-account-",
+    "https://purpleconnect.wemabank.com/support/solutions/articles/67000511713-which-mobile-platforms-are-supported-by-the-alat-application-",
+    # "https://economictimes.indiatimes.com/definition/investment-banking",
+    # "https://otdavid9.github.io/data_analyst_portfolio/"
+    
+
+]
+
+# urls_list  = [
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511710-what-is-alat-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511711-who-can-use-alat-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511712-how-much-does-it-cost-to-get-alat-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511713-which-mobile-platforms-are-supported-by-the-alat-application-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511714-can-i-create-goals-on-alat-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000614930-email-indemnity-form',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511715-what-type-of-goals-can-i-create-on-alat-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511716-what-is-the-highest-amount-i-can-save-on-goals-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511717-can-i-take-loan-on-my-goals-',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511718-how-to-register-on-alat',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511719-wallet-customers-on-alat',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511720-how-to-perform-a-password-reset',
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511721-how-to-create-an-alat-goal', 
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511736-how-to-perform-alat-pin-reset', 
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000511737-how-to-hotlist-my-alat-card', 
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000666718-my-account-is-restricted-what-do-i-do-', 
+#     'https://wemabank.freshdesk.com/support/solutions/articles/67000666720-my-transaction-was-declined-what-can-i-do-'
+    
+#     ]
