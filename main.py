5578205c7ca355580176fe5342eadcd5153642e1
OTDavid <tunjiobidokun@gmail.com>
2024-02-19 11:27:46 +0100

    comment last line main

--- main.py
+++ main.py
@@ -126,6 +126,6 @@ async def process_query(query: Query):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
